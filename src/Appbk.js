f87e11cb0c8fa1614a953fa6a6133466de49a574
Micah <micah@Micahs-Mac-mini.local>
2021-03-18 10:47:52 -0700

    bug fixes3

---
+++ src/Appbk.js
@@ -0,0 +1,21 @@
+import './App.css';
+import ReactPlayer from 'react-player'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button className="button stakenow">Stake Now - Earn 12% APY!</button>
+        <span className="logo">99.99% Uptime - Harmony One Validator</span>
+      </header>
+      <div className="Hero">
+          <ReactPlayer url='cashcow.mp4' playing={true} loop={true} width='100%' height='auto' />
+      </div>
+      <div className="Features">
+        <p>Why CashCow?</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
