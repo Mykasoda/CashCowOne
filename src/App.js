d820000b70c1c660964226a84361f493109f822e
Micah <micah@Micahs-Mac-mini.local>
2021-03-18 00:59:48 -0700

    template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <button className="button stakenow">Stake Now - Earn 12% APY!</button>
+        <span className="logo">99.99% Uptime - Harmony One Validator</span>
       </header>
+      <div className="Hero">
+          <ReactPlayer url='cashcow.mp4' playing={true} loop={true} width='100%' height='auto' />
+      </div>
+      <div className="Features">
+        <p>Why CashCow?</p>
+      </div>
     </div>
   );
 }
