3206d0e6cbe61b3f89893e07a6017cfb9c7661d2
Micah <micah@Micahs-Mac-mini.local>
2021-03-18 22:08:59 -0700

    animation fix

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="https://staking.harmony.one/validators/mainnet/one1vcye9qvlgjrpfmsv0k925x3fq2z4c26nhefx9k" className="button stakenow">Stake Now - Earn 12% APY!</a>
+        <a target="_blank" rel="noreferrer" href="https://staking.harmony.one/validators/mainnet/one1vcye9qvlgjrpfmsv0k925x3fq2z4c26nhefx9k" className="button stakenow">Stake Now - Earn 12% APY!</a>
         <span className="logo">99.99% Uptime - Harmony One Validator</span>
       </header>
       <div className="Hero">
-          <ReactPlayer url='cashcow.mp4' playing={true} loop={true} width='100%' height='auto' />
+          <ReactPlayer url='cashcow.mp4' playbackRate=".6" playing={true} muted={true} loop={true} width='100%' height='auto' />
       </div>
       <div className="Features">
         <p>Why CashCow?</p>
