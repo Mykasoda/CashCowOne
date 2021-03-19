e8f57ab38bd8a784a695e43dcd1f13589958c3fa
Micah <micah@Micahs-Mac-mini.local>
2021-03-18 22:28:30 -0700

    feature cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import ReactPlayer from 'react-player'
+import logo from './logo.svg';
+import stakesmall from './stakesmall.png';
 
 function App() {
   return (
@@ -9,10 +11,46 @@ function App() {
         <span className="logo">99.99% Uptime - Harmony One Validator</span>
       </header>
       <div className="Hero">
-          <ReactPlayer url='cashcow.mp4' playbackRate=".6" playing={true} muted={true} loop={true} width='100%' height='auto' />
+          <ReactPlayer url='cashcow.mp4' playbackRate={0.6} playing={true} muted={true} loop={true} width='100%' height='auto' />
       </div>
+      <h2>Why CashCow?</h2>
       <div className="Features">
-        <p>Why CashCow?</p>
+        <div className="card">
+          <img src={stakesmall} alt="Avatar" className="Avatar" />
+          <div className="container">
+            <h4><b>Stake Small, Get BIG REWARDS</b></h4>
+            <p>Due to Harmony's Effective Proof of Stake system, smaller validators actually get bigger rewards than the average sized ones.</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={logo} alt="Avatar" className="Avatar" />
+          <div className="container">
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={logo} alt="Avatar" className="Avatar" />
+          <div className="container">
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={logo} alt="Avatar" className="Avatar" />
+          <div className="container">
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={logo} alt="Avatar" className="Avatar" />
+          <div className="container">
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+
       </div>
     </div>
   );
